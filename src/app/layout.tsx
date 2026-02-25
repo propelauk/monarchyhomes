6e07c6144ccb22e3f6214ae5da90e156c343ad4c
techmigouk <techmigouk@gmail.com>
2026-02-25 07:09:03 +0000

    Add explicit favicon link tags for better browser compatibility

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,6 +49,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/logo.png" type="image/png" />
+        <link rel="shortcut icon" href="/logo.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/logo.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </head>
